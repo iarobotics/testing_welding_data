0ff943c68d5a4ab335e0cf9de284ca23c557049a
Ion Sircu <sircu.ion@gmail.com>
2019-05-01 01:28:14 +0200

    New trained NN with delay 20

--- predicting_arc_mod.m
+++ predicting_arc_mod.m
@@ -24,21 +24,23 @@ all_voltages = [current_voltage; current_voltage2; current_voltage3];
 all_arcs = [arc; arc2; arc3];
 
 %delay 
-xi1 = all_voltages(1:10, 1:2);
+%xi1 = all_voltages(1:10, 1:2);
+xi1 = all_voltages(1:20, 1:2);
 
 %% Run NN
-[y1,xf1] = myNeuralNetworkFunction_io(all_voltages.',xi1.');
+%[y1,xf1] = myNeuralNetworkFunction_io(all_voltages.',xi1.');
+[y1,xf1] = myNeuralNetworkFunction_10n_20d(all_voltages.',xi1.');
 
 % Threshold the NN y output
-threshold = 0.8;
-y_thresh = y1;
-y_thresh(y1 < threshold) = 0;
-y_thresh(y1 > threshold) = 1;
+%threshold = 0.8;
+%y_thresh = y1;
+%y_thresh(y1 < threshold) = 0;
+%y_thresh(y1 > threshold) = 1;
 
 %plot network output against actual arc for the data used for training
 figure(1)
 %plot(1:length(y1),all_arcs,1:length(y1),y1),
-plot(1:length(y1),all_arcs,1:length(y1),y_thresh),
+plot(1:length(y1),all_arcs,1:length(y1),y1),
 title('Predicted against actual short circuit - Training'),
 grid on
 
@@ -47,23 +49,24 @@ grid on
 %delays for new data
 xi1_test = current_voltage4(1:10, 1:2);
 
-[y_new,xf1_new] = myNeuralNetworkFunction_io(current_voltage4.',xi1_test.');
+%[y_new,xf1_new] = myNeuralNetworkFunction_io(current_voltage4.',xi1_test.');
+[y_new,xf1_new] = myNeuralNetworkFunction_10n_20d(current_voltage4.',xi1_test.');
 
 % Threshold the NN y output
-y_thresh_new = y_new;
-y_thresh_new(y_new < threshold) = 0;
-y_thresh_new(y_new > threshold) = 1;
+%y_thresh_new = y_new;
+%y_thresh_new(y_new < threshold) = 0;
+%y_thresh_new(y_new > threshold) = 1;
 
 %plot network output against actual arc
 figure(2)
 %plot(1:length(y_new),arc4,1:length(y_new),y_new),
-plot(1:length(y_new),arc4,1:length(y_new),y_thresh_new),
+plot(1:length(y_new),arc4,1:length(y_new),y_new),
 title('Predicted against actual short circuit - Testing'),
 grid on
 
 
 %% Save variables to file
-threshold = 0.4;
+threshold = 0.65;
 y_thresh = y1;
 y_thresh(y1 < threshold) = 0;
 y_thresh(y1 > threshold) = 1;
