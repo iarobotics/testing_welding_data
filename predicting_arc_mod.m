694d128ea24f905b919191a4bc91ebc8e137295e
Ion Sircu <sircu.ion@gmail.com>
2019-04-30 21:43:47 +0200

    Added threshold for NN output

--- predicting_arc_mod.m
+++ predicting_arc_mod.m
@@ -1,7 +1,7 @@
+%% Load data
 clear all
 clc
 
-%% Import and save data
 log1 = importdata('log1.txt');
 %current =log1(0.5e5:end,1);
 current_voltage = log1(0.5e5:end,1:2);
@@ -15,53 +15,51 @@ arc2 = log2(0.5e5:end,3);
 log3=importdata('log3.txt');
 current_voltage3 = log3(0.5e5:end,1:2);
 arc3 = log3(0.5e5:end,3);
-save('all_voltages', 'current_voltage', 'current_voltage2', 'current_voltage3');
-save('all_arcs', 'arc', 'arc2', 'arc3');
 
-%% Load variables
-% evaluate this selection one by one because cell2mat fails
-all_voltages=load('all_voltages.mat');
-all_voltages=struct2cell(all_voltages);
-all_voltages=cell2mat(all_voltages);
-
-all_arcs=load('all_arcs.mat');
-all_arcs=struct2cell(all_arcs);
-all_arcs=cell2mat(all_arcs);
-
-%output=load('nnet_3files_output.mat');
-%output=struct2cell(output);
-%output=cell2mat(output);
-
-%delay 
-xi1 = all_voltages(1:10, 1:2);
-xi2 = all_arcs(1:10);
-
-[y1,xf1] = myNeuralNetworkFunction_io(all_voltages.',xi1.');
-
-%plot network output against actual arc
-figure(12)
-plot(1:length(y1),all_arcs,1:length(y1),y1)
-
-
-%testing the network on new data
 log4 = importdata('log4.txt');
 current_voltage4 = log4(0.5e5:end,1:2);
 arc4 = log4(0.5e5:end,3);
 
+all_voltages = [current_voltage; current_voltage2; current_voltage3];
+all_arcs = [arc; arc2; arc3];
+
+%delay 
+xi1 = all_voltages(1:10, 1:2);
+
+%% Run NN
+[y1,xf1] = myNeuralNetworkFunction_io(all_voltages.',xi1.');
+
+% Threshold the NN y output
+threshold = 0.8;
+y_thresh = y1;
+y_thresh(y1 < threshold) = 0;
+y_thresh(y1 > threshold) = 0.9;
+
+%plot network output against actual arc for the data used for training
+figure(1)
+%plot(1:length(y1),all_arcs,1:length(y1),y1),
+plot(1:length(y1),all_arcs,1:length(y1),y_thresh),
+title('Predicted against actual short circuit - Training'),
+grid on
+
+
+%testing the network on new data
 %delays for new data
 xi1_test = current_voltage4(1:10, 1:2);
-xi2_test = arc4(1:10);
 
 [y_new,xf1_new] = myNeuralNetworkFunction_io(current_voltage4.',xi1_test.');
 
+% Threshold the NN y output
+y_thresh_new = y_new;
+y_thresh_new(y_new < threshold) = 0;
+y_thresh_new(y_new > threshold) = 0.9;
+
 %plot network output against actual arc
-figure(12)
-plot(1:length(y_new),arc4,1:length(y_new),y_new)
+figure(2)
+%plot(1:length(y_new),arc4,1:length(y_new),y_new),
+plot(1:length(y_new),arc4,1:length(y_new),y_thresh_new),
+title('Predicted against actual short circuit - Testing'),
+grid on
 
-
-%save('current.mat','current', 'current2');
-%currents=load('current.mat');
-
- %plot(1:length(arc2),arc(1:length(arc2)),1:length(arc2),arc2)
  
  
