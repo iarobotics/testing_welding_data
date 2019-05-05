e2d3813c895268e81e6f87bf5bdecd4e55c54319
Amalia Cretu <lelia.cretu@gmail.com>
2019-05-05 19:57:24 +0200

    nn for testing delays for fct 1

--- nnet_10pp.m
+++ nnet_10pp.m
@@ -21,20 +21,27 @@ log4 = importdata('data_4_cut_first_90p.txt');
 current_voltage4 = log4(:,1:2);
 arc4 = log4(:,3);
 
+arc_full = importdata('log4.txt');
+arc_full = arc_full(0.5e5:end,3);
+
 all_voltages_10pp = [current_voltage; current_voltage2; current_voltage3];
 all_arcs_10pp = [arc; arc2; arc3];
 
 %delay 
-xi1_10pp = current_voltage4(1:10, 1:2);
+xi1_10pp = current_voltage4(1:20, 1:2);
 %xi1_10pp = all_voltages(1:20, 1:2);
 
-[y_new,xf1_new] = nn_d4n10(current_voltage4.',xi1_10pp.');
+[y_new,xf1_new] = nn_d20n10(current_voltage4.',xi1_10pp.');
 
 %% Save variables to file
 threshold = 0.65;
 y_thresh = y_new;
 y_thresh(y_new < threshold) = 0;
-y_thresh(y_new > threshold) = 1;
+y_thresh(y_new > threshold) = 0.9;
+
+plot(1:length(y_thresh)+2,arc_full,1:length(y_thresh),y_thresh),
+title('Predicted against actual short circuit - Training'),
+grid on
 
 % save thresholded output
 ys = y_thresh';
